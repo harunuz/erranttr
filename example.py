71f7360aeaf55ca456a11fe16a2d98ca9eb8cc64
harun <harunuz466@hotmail.com>
2022-12-06 23:57:46 +0300

    added evaluation scripts

--- example.py
+++ example.py
@@ -5,19 +5,21 @@ annotator = erranttr.load('tr')
 # m = TurkishMorphology.create_with_defaults()
 
 origs = [
-    "Ne yani dişarı çikabilirmiyiz?",
-    "Halbuki ben çok sevmişdim onu",
-    "Bence dünyada salgın hastalıkların oldumağı yer mümkündür.",
-    "Günümüzde çok hastaliklerle yaşamaya devam ediyoruz.",
-    "Sağlığımizi korumak için ne yapmaliyiz."
+    # "Ne yani dişarı çikabilirmiyiz?",
+    # "Halbuki ben çok sevmişdim onu",
+    # "Bence dünyada salgın hastalıkların oldumağı yer mümkündür.",
+    # "Günümüzde çok hastaliklerle yaşamaya devam ediyoruz.",
+    # "Sağlığımizi korumak için ne yapmaliyiz.",
+    "Sonunda yabancı bir ülkelere yasmak için gitseydik, olumlu ve olumsuz hayatın özellikleri gözlerimizin önünde koymalıyız."
 ]
 
 corrs = [
-    "Ne yani dışarı çıkabilir miyiz?",
-    "Halbuki ben çok sevmiştim onu,",
-    "Bence dünyada salgın hastalıkların olmadığı yer mümkündür.",
-    "Günümüzde pek çok hastalıkla yaşamaya devam ederiz.",
-    "Sağlığımızı korumak için ne yapmalıyız?"
+    # "Ne yani dışarı çıkabilir miyiz?",
+    # "Halbuki ben çok sevmiştim onu",
+    # "Bence dünyada salgın hastalıkların olmadığı yer mümkündür.",
+    # "Günümüzde pek çok hastalıkla yaşamaya devam ederiz.",
+    # "Sağlığımızı korumak için ne yapmalıyız?",
+    "Sonunda yabancı bir ülkeye yaşamak için gitseydik, olumlu ve olumsuz hayatın özelliklerini gözlerimizin önüne koymalıyız."
 ]
 
 print("====================================================================================\n")
